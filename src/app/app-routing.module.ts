5d2f1f3f9c4438c27eb60594ebbf895f043ed1af
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-07-19 21:00:53 -0600

    [ADD] Temporal Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CategoryComponent } from './category/category.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CompanyComponent } from './company/company.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'home', component: CategoryComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'company', component: CompanyComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

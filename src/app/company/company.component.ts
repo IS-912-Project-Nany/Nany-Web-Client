89a9718056b45c08cae2d46b6f6a5b17adf53939
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-07-25 14:36:49 -0600

    [FIX] Img Positions

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -2,13 +2,48 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-company',
-  templateUrl: './company.component.html'
+  templateUrl: './company.component.html',
 })
 export class CompanyComponent implements OnInit {
+  companies = [
+    {
+      name: 'Little Caesars',
+      logo: '../../assets/img/companies/empresa-little-caesars.svg',
+    },
+    {
+      name: 'Bigos',
+      logo: '../../assets/img/companies/empresa-bigos.jpg',
+    },
+    {
+      name: 'Pizza Hut',
+      logo: '../../assets/img/companies/empresa-pizza-hut.svg',
+    },
+    {
+      name: 'Pollo Campero',
+      logo: '../../assets/img/companies/empresa-Pollo-Capero-logo.png',
+    },
+    {
+      name: 'KFC',
+      logo: '../../assets/img/companies/empresa-kfc.png',
+    },
+    {
+      name: "Wendy's",
+      logo: '../../assets/img/companies/empresa-wendys.jpg',
+    },
+    {
+      name: 'Popeyes',
+      logo: '../../assets/img/companies/empresa-Popeyes.png',
+    },
+    {
+      name: 'McDonalds',
+      logo: '../../assets/img/companies/empresa-McDonalds.png',
+    },
+    {
+      name: 'Burger King',
+      logo: '../../assets/img/companies/empresa-Burger-King-Logo.png',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,9 +8,46 @@ import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
   templateUrl: './product.component.html',
 })
 export class ProductComponent implements OnInit {
+  company = {
+    name: "Wendy's",
+    description:
+      'La marca Wendy´s se caracteriza por su espíritu innovador lo cual va desde nuevos productos y lanzamientos hasta las mejoras de image de las instalaciones que son frecuentes para tener una marca renovada',
+    abstract: 'Restaurante de comida rápida y hamburguesería',
+    logo: '../../assets/img/companies/empresa-wendys.jpg',
+    schedule: 'Lunes a Viernes: 8:00am a 10:00pm \n Sábados y Domingos: 10:00am a 3:00pm \n',
+    location: 'Blvd Morazan Mall Multiplaza Plaza Miraflores',
+    banner: '../../assets/img/banners/banner-wendys.jpg',
+    rating: 4,
+  };
+
+  products = [
+    {
+      name: 'Coca Cola Vidrio',
+      description: 'tamaño 25cm. Vidrio reutilizable.',
+      isv: 5.0,
+      price: 10.0,
+      stock: 10,
+      image: '../assets/img/products/Coca-Cola-bebida.webp',
+    },
+    {
+      name: 'Coca Cola Vidrio',
+      description: 'tamaño 25cm. Vidrio reutilizable.',
+      isv: 5.0,
+      price: 10.00,
+      stock: 10,
+      image: '../assets/img/products/Coca-Cola-bebida.webp',
+    },
+    {
+      name: 'Coca Cola Vidrio',
+      description: 'tamaño 25cm. Vidrio reutilizable.',
+      isv: 5.0,
+      price: 10.0,
+      stock: 10,
+      image: '../assets/img/products/Coca-Cola-bebida.webp',
+    },
+  ];
   constructor(library: FaIconLibrary) {
     library.addIcons(fasStar, farStar);
   }
-
   ngOnInit(): void {}
 }

da648b307343d06f66399d253c6e54a14f528644
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-07-31 23:26:30 -0600

    [ADD] Json Temporal

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,13 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit {
+  orders: any = [
+    {
+      destination: 'Col. La Travesia',
+      buyer: 'Marcos Torres',
+      total: 990.00,
+      productName: 'Wendys',
+      imgOrder: '../../assets/img/products/wendys-food.jpg',
+      date:'12/20/2021',
+      state: {
+        name: "En Camino",
+        color: '#FF1E1E'
+      }
+    },
+    {
+      destination: 'Col. La Travesia',
+      buyer: 'Marcos Torres',
+      date:'12/20/2021',
+      total: 990.00,
+      imgOrder: '../../assets/img/products/wendys-food.jpg',
+      state: {
+        name: "Entregado",
+        color: '#00EF35'
+      }
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

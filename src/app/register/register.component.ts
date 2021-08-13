b8743d3e7c16b0c94bed8587723dbad620d3c3d7
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-13 11:35:13 -0600

    [ADD] Redirect Registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,55 +1,134 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html'
 })
 export class RegisterComponent implements OnInit {
-  formRegister = new FormGroup({
-    name: new FormControl ('', [Validators.required, Validators.maxLength(30)]),
-    lastName: new FormControl ('', [Validators.required, Validators.maxLength(30)]),
-    email: new FormControl('', [Validators.required,Validators.pattern(
+  formRegistro = new FormGroup({
+    nombre: new FormControl ('', [Validators.required, Validators.maxLength(30)]),
+    apellido: new FormControl ('', [Validators.required, Validators.maxLength(30)]),
+    correo: new FormControl('', [Validators.required,Validators.pattern(
         /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i),]),
     password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([A-Za-z\d$@$!%*?&]){8,}$/)]),
-    city: new FormControl('', [Validators.required]),
-    birthday: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required])
+    ciudad: new FormControl('', [Validators.required]),
+    fechaNacimiento: new FormControl('', [Validators.required]),
+    genero: new FormControl('', [Validators.required])
   });
-
   confirmPassword = new FormControl('',[Validators.required]);
+  responseRegistro: any = '';
+  ciudades = [
+    { idCiudad: 1, ciudad: 'Choluteca'},
+    { idCiudad: 2, ciudad: 'Comayagua'},
+    { idCiudad: 3, ciudad: 'Gracias'},
+    { idCiudad: 4, ciudad: 'Juticalpa'},
+    { idCiudad: 5, ciudad: 'La Ceiba'},
+    { idCiudad: 6, ciudad: 'La Paz'},
+    { idCiudad: 7, ciudad: 'La Esperanza'},
+    { idCiudad: 8, ciudad: 'Nacaome'},
+    { idCiudad: 9, ciudad: 'Nuevo Ocotepeque'},
+    { idCiudad: 10, ciudad: 'Puerto Lempira'},
+    { idCiudad: 11, ciudad: 'Roatán'},
+    { idCiudad: 12, ciudad: 'San Pedro Sula'},
+    { idCiudad: 13, ciudad: 'Santa Bárbara'},
+    { idCiudad: 14, ciudad: 'Santa  Rosa de Copán'},
+    { idCiudad: 15, ciudad: 'Tegucigalpa'},
+    { idCiudad: 16, ciudad: 'Trujillo'},
+    { idCiudad: 17, ciudad: 'Yoro'},
+    { idCiudad: 18, ciudad: 'Yuscarán'}
+  ]
 
-  constructor() { }
+  constructor(
+    private usuariosService: UsuariosService,
+    private cookiesService:CookieService,
+    private _route: Router
+    ) { }
 
   ngOnInit() {
   }
 
-  get name(){
-    return this.formRegister.get('name');
+  registrar() {
+    let ciudadSeleccionada: any = {};
+    this.ciudades.forEach(ciudad => {
+      if(this.formRegistro.value.ciudad == ciudad.idCiudad){
+        ciudadSeleccionada.idCiudad = this.formRegistro.value.ciudad;
+        ciudadSeleccionada.ciudad = ciudad.ciudad;
+      }
+    });
+
+    let nuevoUsuario = {
+      nombre: this.formRegistro.value.nombre,
+      apellido: this.formRegistro.value.apellido,
+      correo: this.formRegistro.value.correo,
+      password: this.formRegistro.value.password,
+      fechaNacimiento: this.formRegistro.value.fechaNacimiento,
+      genero: this.formRegistro.value.genero,
+      tipoUsuario: { idUsuario: 1, tipo: "cliente"},
+      imagen: "../../assets/iconos/user-icon.png",
+      ciudad: ciudadSeleccionada,
+      ordenes: []
+    }
+    console.log("Se registrara al usuario:", nuevoUsuario);
+
+    this.usuariosService.registrar(nuevoUsuario).subscribe(
+      result => {
+        if (result.code == 0) {
+          this.responseRegistro = result;
+          this.correo.setValue('');
+        } else {
+          const dateNow = new Date();
+          dateNow.setMinutes(dateNow.getMinutes() + 60);
+          this.cookiesService.set('nanyUsuarioId', result.usuario._id, dateNow);
+          this.cookiesService.set(
+            'nanyUsuarioNombre',
+            result.usuario.nombre,
+            dateNow
+          );
+          this.cookiesService.set(
+            'nanyUsuarioApellido',
+            result.usuario.apellido,
+            dateNow
+          );
+
+          this._route.navigate(['/home']);
+        }
+        console.log(result);
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
-  get lastName(){
-    return this.formRegister.get('lastName');
+  get nombre(){
+    return this.formRegistro.get('nombre');
   }
 
-  get email() {
-    return this.formRegister.get('email');
+  get apellido(){
+    return this.formRegistro.get('apellido');
+  }
+
+  get correo() {
+    return this.formRegistro.get('correo');
   }
 
   get password() {
-    return this.formRegister.get('password');
+    return this.formRegistro.get('password');
   }
 
-  get city(){
-    return this.formRegister.get('city');
+  get ciudad(){
+    return this.formRegistro.get('ciudad');
   }
 
-  get birthday(){
-    return this.formRegister.get('birthday');
+  get fechaNacimiento(){
+    return this.formRegistro.get('fechaNacimiento');
   }
 
-  get gender(){
-    return this.formRegister.get('gender');
+  get genero(){
+    return this.formRegistro.get('genero');
   }
-
 }

c00820f440ccf4b91ade755b232124b794076144
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-14 10:41:50 -0600

    [ADD] Ordenes Service

--- src/app/services/ordenes.service.ts
+++ src/app/services/ordenes.service.ts
@@ -1,9 +1,16 @@
+import { API_BASE_URL } from '../app.config';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdenesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  obtenerOrdenes(idUsuario: String):Observable<any> {
+    return this.httpClient.get(`${API_BASE_URL}/usuarios/${idUsuario}`)
+  }
 }

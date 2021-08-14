df63015c9abea040cf889d03a89123538ee88481
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-14 10:42:37 -0600

    [FIX] Details

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -36,31 +36,6 @@ export class PerfilComponent implements OnInit {
     { idCiudad: 17, ciudad: 'Yoro'},
     { idCiudad: 18, ciudad: 'Yuscarán'}
   ]
-  orders: any = [
-    {
-      destination: 'Col. La Travesia',
-      buyer: 'Marcos Torres',
-      total: 990.00,
-      productName: 'Wendys',
-      imgOrder: '../../assets/img/products/wendys-food.jpg',
-      date:'12/20/2021',
-      state: {
-        name: "En Camino",
-        color: '#FF1E1E'
-      }
-    },
-    {
-      destination: 'Col. La Travesia',
-      buyer: 'Marcos Torres',
-      date:'12/20/2021',
-      total: 990.00,
-      imgOrder: '../../assets/img/products/wendys-food.jpg',
-      state: {
-        name: "Entregado",
-        color: '#00EF35'
-      }
-    },
-  ];
   constructor(
     private usuariosService:UsuariosService,
     private cookieService:CookieService,

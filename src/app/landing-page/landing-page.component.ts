e6faf013291c933442245a0cbcb8a1257a8dd3c0
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-13 23:37:26 -0600

    [ADD] Details Landing Page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { UploadService } from '../services/upload.service';
 
 @Component({
@@ -6,33 +7,13 @@ import { UploadService } from '../services/upload.service';
   templateUrl: './landing-page.component.html'
 })
 export class LandingPageComponent implements OnInit {
-  //formData = new FormData();
-  constructor(private uploadService:UploadService) { }
+  isLogged: boolean = false;
+
+  constructor(private cookiesService: CookieService) { }
 
   ngOnInit(): void {
+    if (this.cookiesService.check('nanyUsuarioId')) {
+      this.isLogged = true;
+    }
   }
-
-  // importFile(event) {
-  // if (event.target.files.length == 0) {
-  //     console.log("No file selected!");
-  //     return
-  // }
-  //   let file: File = event.target.files[0];
-  //   console.log(file);
-  //   this.formData.append("imagen", file);
-  //   this.formData.append("folder", 'usuarios');
-  // }
-
-  // subir() {
-  //   console.log();
-  //   this.uploadService.subirImagen(this.formData).subscribe(
-  //     result => {
-  //       console.log(result);
-  //     },
-  //     error => {
-  //       console.log(error);
-  //     }
-  //   );
-  // }
-
 }

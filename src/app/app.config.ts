c7cc7abc7433db1fdf6f9ec03df53483e1d6b7b4
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-18 23:44:39 -0600

    [ADD] Env Vars

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,3 @@
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 export const API_BASE_URL =  environment.ROOT_API /* 'http://localhost:8888'*/;

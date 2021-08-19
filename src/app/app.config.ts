0ac7982a07a7a9a6081165bb387fdb8dce6025ad
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2021-08-19 00:25:24 -0600

    [FIX] Details

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,1 @@
-import { environment } from '../environments/environment.prod';
-
-export const API_BASE_URL =  environment.ROOT_API /* 'http://localhost:8888'*/;
+export const API_BASE_URL =  'https://nany-rest-api.herokuapp.com' /* 'http://localhost:8888'*/;
